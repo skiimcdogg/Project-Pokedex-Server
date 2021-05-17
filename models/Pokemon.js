8521b53bc39b9fd77585bfaab85b9568386a44dd
Pauline B <pauline.bellaud@gmail.com>
2021-05-17 12:05:22 +0200

    models updated

---
+++ models/Pokemon.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const pokeSchema = new Schema({
+
+        name: String,
+        types: [String],
+        height: Number,
+        weight: Number,
+        stats: [String],
+        base_stat: [String],
+        moves: [String],
+        image: String
+
+})
+
+const Pokemon = mongoose.model("pokemons", pokeSchema);
+module.exports = Pokemon;
--- models/User.js
+++ models/User.js
@@ -2,10 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  pseudo: String,
   email: { type: String, required: true },
   password: { type: String, required: true },
+  avatar: String,
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  region: {
+    type: String,
+    enum: ["Kanto", "Johto", "Hoenn", "Sinnoh", "Unys", "Kalos", "Alola", "Galar"],
+  },
+  pokeFav: [{
+      type: Schema.Types.ObjectId,
+      ref: "pokemons",
+    }],
+  pokeTeam: [{
+      type: Schema.Types.ObjectId,
+      ref: "pokemons",
+    }]
 });
 
 const User = mongoose.model("User", userSchema);
-
 module.exports = User;
