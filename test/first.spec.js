226895e8671d22a774785e12a619f63cdfd2d7bf
Cl Sayart <clsayart@gmail.com>
2021-06-01 21:47:29 +0200

    test micha

---
+++ test/first.spec.js
@@ -0,0 +1,40 @@
+
+process.env.NODE_ENV = 'test';
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require ("../app")
+
+
+let mongoose = require("mongoose");
+let Book = require('../models/Pokemon');
+
+
+//Assertion Style
+chai.should();
+
+chai.use(chaiHttp);
+
+describe('Tasks API', () => {
+
+    /**
+     * Test the GET route
+     */
+    
+     describe('/GET pokemon', () => {
+        it('it should GET all the pokemons', (done) => {
+          chai.request(server)
+              .get('/api/pokemons')
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(27);
+                done();
+              });
+        });
+    });
+
+
+});
+
+
