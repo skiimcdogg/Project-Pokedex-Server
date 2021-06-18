f4e4f0d0c7096af3d2c1d714eebfd55a22f50f78
Antoine Stouff <antoine-78280@hotmail.fr>
2021-06-18 14:56:53 +0200

    test

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
 // });
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, './public/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
