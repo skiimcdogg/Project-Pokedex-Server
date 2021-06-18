2517fec4c5a8d0cb04763d2d115a2051252035a0
Antoine Stouff <antoine-78280@hotmail.fr>
2021-06-18 14:54:33 +0200

    test

--- app.js
+++ app.js
@@ -84,4 +84,12 @@ app.use((err, req, res, next) => {
 //   res.sendFile(__dirname + "../public/index.html");
 // });
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 module.exports = app;
